8cb0544e02b1f591e9f2faacf8c345d3a34a6b2c
Vlad Petriv <vladpetriv05@gmail.com>
2022-05-02 20:18:46 +0300

    Test:update rows in mock because updated channel model

--- internal/store/pg/channel_test.go
+++ internal/store/pg/channel_test.go
@@ -1,7 +1,6 @@
 package pg_test
 
 import (
-	"database/sql"
 	"testing"
 
 	sqlmock "github.com/DATA-DOG/go-sqlmock"
@@ -91,19 +90,19 @@ func TestChannelPg_GetChannel(t *testing.T) {
 		{
 			name: "Ok",
 			mock: func() {
-				rows := sqlmock.NewRows([]string{"id", "name"}).
-					AddRow(1, "test")
+				rows := sqlmock.NewRows([]string{"id", "name", "title", "photourl"}).
+					AddRow(1, "test", "test", "test.jpg")
 
 				mock.ExpectQuery("SELECT * FROM channel WHERE id=$1;").
 					WithArgs(1).WillReturnRows(rows)
 			},
 			input: 1,
-			want:  &model.Channel{ID: 1, Name: "test"},
+			want:  &model.Channel{ID: 1, Name: "test", Title: "test", PhotoURL: "test.jpg"},
 		},
 		{
 			name: "channel not found",
 			mock: func() {
-				rows := sqlmock.NewRows([]string{"id", "name"})
+				rows := sqlmock.NewRows([]string{"id", "name", "title", "photourl"})
 
 				mock.ExpectQuery("SELECT * FROM channel WHERE id=$1;").
 					WithArgs(404).WillReturnRows(rows)
@@ -118,65 +117,6 @@ func TestChannelPg_GetChannel(t *testing.T) {
 			tt.mock()
 
 			got, err := r.GetChannel(tt.input)
-			if tt.wantErr {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tt.want, got)
-			}
-
-			assert.NoError(t, mock.ExpectationsWereMet())
-		})
-	}
-}
-
-func TestChannelPg_GetChannels(t *testing.T) {
-	db, mock, err := utils.CreateMock()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-
-	defer db.Close()
-
-	r := pg.NewChannelRepo(&pg.DB{DB: db})
-
-	tests := []struct {
-		name    string
-		mock    func()
-		want    []model.Channel
-		wantErr bool
-	}{
-		{
-			name: "Ok",
-			mock: func() {
-				rows := sqlmock.NewRows([]string{"id", "name"}).
-					AddRow(1, "test1").
-					AddRow(2, "test2")
-
-				mock.ExpectQuery("SELECT * FROM channel;").
-					WillReturnRows(rows)
-			},
-			want: []model.Channel{
-				{ID: 1, Name: "test1"},
-				{ID: 2, Name: "test2"},
-			},
-		},
-		{
-			name: "channels not found",
-			mock: func() {
-				rows := sqlmock.NewRows([]string{"id", "name"})
-				mock.ExpectQuery("SELECT * FROM channel;").
-					WillReturnRows(rows)
-			},
-			wantErr: true,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.mock()
-
-			got, err := r.GetChannels()
 			if tt.wantErr {
 				assert.Error(t, err)
 			} else {
@@ -209,19 +149,19 @@ func TestChannelPg_GetByName(t *testing.T) {
 		{
 			name: "Ok",
 			mock: func() {
-				rows := sqlmock.NewRows([]string{"id", "name"}).
-					AddRow(1, "test")
+				rows := sqlmock.NewRows([]string{"id", "name", "title", "photourl"}).
+					AddRow(1, "test", "test", "test.jpg")
 
 				mock.ExpectQuery("SELECT * FROM channel WHERE name=$1;").
 					WithArgs("test").WillReturnRows(rows)
 			},
 			input: "test",
-			want:  &model.Channel{ID: 1, Name: "test"},
+			want:  &model.Channel{ID: 1, Name: "test", Title: "test", PhotoURL: "test.jpg"},
 		},
 		{
 			name: "channel not found",
 			mock: func() {
-				rows := sqlmock.NewRows([]string{"id", "name"})
+				rows := sqlmock.NewRows([]string{"id", "name", "title", "photourl"})
 
 				mock.ExpectQuery("SELECT * FROM channel WHERE name=$1;").
 					WithArgs().WillReturnRows(rows)
@@ -240,57 +180,6 @@ func TestChannelPg_GetByName(t *testing.T) {
 			} else {
 				assert.NoError(t, err)
 				assert.Equal(t, tt.want, got)
-			}
-
-			assert.NoError(t, mock.ExpectationsWereMet())
-		})
-	}
-}
-
-func TestChannelPg_DeleteChannel(t *testing.T) {
-	db, mock, err := utils.CreateMock()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-
-	defer db.Close()
-
-	r := pg.NewChannelRepo(&pg.DB{DB: db})
-
-	tests := []struct {
-		name    string
-		mock    func()
-		input   int
-		wantErr bool
-	}{
-		{
-			name: "Ok",
-			mock: func() {
-				mock.ExpectExec("DELETE FROM channel WHERE id=$1;").
-					WithArgs(1).WillReturnResult(sqlmock.NewResult(0, 1))
-			},
-			input: 1,
-		},
-		{
-			name: "channel not found",
-			mock: func() {
-				mock.ExpectExec("DELETE FROM channel WHERE id=$1;").
-					WithArgs(404).WillReturnError(sql.ErrNoRows)
-			},
-			input:   404,
-			wantErr: true,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.mock()
-
-			err := r.DeleteChannel(tt.input)
-			if tt.wantErr {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
 			}
 
 			assert.NoError(t, mock.ExpectationsWereMet())
