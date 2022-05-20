66503a861ec3f22c0e19b5cc3cb47f25350d5a2a
Vlad Petriv <vladpetriv05@gmail.com>
2022-05-20 16:23:44 +0300

    model: update message model because add more info about that

--- internal/model/message.go
+++ internal/model/message.go
@@ -1,8 +1,9 @@
 package model
 
 type Message struct {
-	ID        int    `json:"id"`
-	ChannelID int    `json:"channelId`
-	UserID    int    `json:"userId"`
-	Title     string `json:"title"`
+	ID           int    `json:"id"`
+	ChannelID    int    `json:"channelId"`
+	UserID       int    `json:"userId"`
+	Title        string `json:"title"`
+	RepliesCount int
 }
