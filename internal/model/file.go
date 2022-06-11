df1b32ec02f38bbe6ca05384c228a1277797c223
Vlad Petriv <vladpetriv05@gmail.com>
2022-06-11 11:58:02 +0300

    chore: update models

--- internal/model/file.go
+++ internal/model/file.go
@@ -1,5 +1,11 @@
 package model
 
+import "github.com/gotd/td/tg"
+
 type Image struct {
 	Bytes []byte
+}
+
+type Media struct {
+	Photo *tg.Photo `json:"Photo"`
 }
--- internal/model/message.go
+++ internal/model/message.go
@@ -1,25 +1,22 @@
 package model
 
-import (
-	"github.com/gotd/td/tg"
-)
-
 type Message struct {
 	ID           int    `json:"id"`
 	ChannelID    int    `json:"channelId"`
 	UserID       int    `json:"userId"`
 	Title        string `json:"title"`
 	MessageURL   string `json:"messageURL"`
+	Image        string `json:"image"`
 	RepliesCount int
 }
 
 type TgMessage struct {
 	ID      int
 	Message string
-	FromID  TgUser
-	PeerID  TgChannel
-	Replies TgReplies
-	ReplyTo TgReplyTo
-	Media   tg.MessageMediaPhoto
+	FromID  TgUser    `json:"FromID"`
+	PeerID  TgChannel `json:"PeerID"`
+	Replies TgReplies `json:"Replies"`
+	ReplyTo TgReplyTo `json:"ReplyTo"`
+	Media   Media     `json:"Media"`
 	Image   *Image
 }
