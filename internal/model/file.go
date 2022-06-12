8f6b5ca03bc0d2021e49566c5df6236b8e59e01d
Vlad Petriv <vladpetriv05@gmail.com>
2022-06-12 14:47:12 +0300

    feat: update model for avoiding empty Sizes field in Photo model

--- internal/model/file.go
+++ internal/model/file.go
@@ -5,7 +5,13 @@ import "github.com/gotd/td/tg"
 type Image struct {
 	Bytes []byte
 }
-
 type Media struct {
-	Photo *tg.Photo `json:"Photo"`
+	Photo *Photo `json:"Photo"`
+}
+
+type Photo struct {
+	ID            int
+	AccessHash    int
+	FileReference []byte
+	Sizes         []tg.PhotoSize
 }
