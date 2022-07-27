a491cbba67820a29e2855c3017fd669c00f19646
Vlad Petriv <vladpetriv05@gmail.com>
2022-07-27 19:52:57 +0300

    feat: remove useless models

--- internal/model/message.go
+++ internal/model/message.go
@@ -1,21 +1,13 @@
 package model
 
-type Message struct {
-	ID           int    `db:"id"`
-	ChannelID    int    `db:"channel_id"`
-	UserID       int    `db:"user_id"`
-	Title        string `db:"title"`
-	MessageURL   string `db:"message_url"`
-	ImageURL     string `db:"imageurl"`
-	RepliesCount int    `db:"count"`
-}
-
 type TgMessage struct {
-	ID      int
-	Message string
-	FromID  TgUser    `json:"FromID"`
-	PeerID  TgChannel `json:"PeerID"`
-	Replies TgReplies `json:"Replies"`
-	ReplyTo TgReplyTo `json:"ReplyTo"`
-	Media   Media     `json:"Media"`
+	ID         int
+	Message    string
+	FromID     TgUser    `json:"FromID"`
+	PeerID     TgChannel `json:"PeerID"`
+	Replies    TgReplies `json:"Replies"`
+	ReplyTo    TgReplyTo `json:"ReplyTo"`
+	Media      Media     `json:"Media"`
+	MessageURL string
+	ImageURL   string
 }
--- internal/model/replie.go
+++ internal/model/replie.go
@@ -1,24 +1,17 @@
 package model
 
-type Replie struct {
-	ID        int    `db:"id"`
-	MessageID int    `db:"message_id"`
-	UserID    int    `db:"user_id"`
-	Title     string `db:"title"`
-	ImageURL  string `db:"imageurl"`
-}
-
 type TgReplies struct {
 	Count    int
 	Messages []TgRepliesMessage
 }
 
 type TgRepliesMessage struct {
-	ID      int
-	FromID  TgUser
-	Message string
-	ReplyTo interface{}
-	Media   Media
+	ID       int
+	FromID   TgUser
+	Message  string
+	ReplyTo  interface{}
+	Media    Media
+	ImageURL string
 }
 
 type TgReplyTo struct {
