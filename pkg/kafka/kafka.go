f92dee1152c0f1f43d8eb2fb34cb43539d6908cb
VladPetriv <vladpetriv05@gmail.com>
2022-09-04 16:24:48 +0300

    refactor: move kafka into pkg

---
+++ pkg/kafka/kafka.go
@@ -0,0 +1,48 @@
+package kafka
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Shopify/sarama"
+)
+
+func PushDataToQueue(topic, addr string, data interface{}) error {
+	producer, err := connectAsProducer(addr)
+	if err != nil {
+		return fmt.Errorf("failed to connect as producer: %w", err)
+	}
+
+	defer producer.Close()
+
+	encodedData, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("failed to marshal data: %w", err)
+	}
+
+	queueMessage := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(encodedData),
+	}
+
+	_, _, err = producer.SendMessage(queueMessage)
+	if err != nil {
+		return fmt.Errorf("failed send message into kafka: %w", err)
+	}
+
+	return nil
+}
+
+func connectAsProducer(addr string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForLocal
+	config.Producer.Retry.Max = 5
+
+	conn, err := sarama.NewSyncProducer([]string{addr}, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create producer: %w", err)
+	}
+
+	return conn, nil
+}
