329eb210a9e0cc655ec99dfa72c352bd7c86be78
VladPetriv <vladpetriv05@gmail.com>
2022-09-17 22:42:28 +0300

    style: change error name

--- pkg/errors/error.go
+++ pkg/errors/error.go
@@ -11,11 +11,11 @@ func (c *CreateError) Error() string {
 	return fmt.Sprintf("create %s error: %s", c.Name, c.ErrorValue)
 }
 
-type GettingError struct {
+type GetError struct {
 	Name       string
 	ErrorValue error
 }
 
-func (g *GettingError) Error() string {
+func (g *GetError) Error() string {
 	return fmt.Sprintf("get %s error: %s", g.Name, g.ErrorValue)
 }
