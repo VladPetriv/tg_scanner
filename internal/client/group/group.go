84c21a5a39a3be3567fac1f5fc87e15fac408eb5
Vlad Petriv <vladpetriv05@gmail.com>
2022-11-06 14:13:25 +0200

    refactor: group logs and errors

--- internal/client/group/group.go
+++ internal/client/group/group.go
@@ -3,12 +3,12 @@ package group
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/gotd/td/tg"
 
 	"github.com/VladPetriv/tg_scanner/internal/client/photo"
 	"github.com/VladPetriv/tg_scanner/internal/model"
-	"github.com/VladPetriv/tg_scanner/pkg/errors"
 	"github.com/VladPetriv/tg_scanner/pkg/logger"
 )
 
@@ -27,12 +27,14 @@ func New(log *logger.Logger, api *tg.Client) *tgGroup {
 }
 
 func (g tgGroup) GetGroups(ctx context.Context) ([]model.TgGroup, error) {
+	logger := g.log
+
 	groups := make([]model.TgGroup, 5)
 
 	data, err := g.api.MessagesGetAllChats(ctx, []int64{})
 	if err != nil {
-		g.log.Error().Err(err)
-		return nil, &errors.GetError{Name: "groups", ErrorValue: err}
+		logger.Error().Err(err).Msg("get all groups")
+		return nil, fmt.Errorf("get all groups error: %w", err)
 	}
 
 	for _, groupData := range data.GetChats() {
@@ -42,13 +44,15 @@ func (g tgGroup) GetGroups(ctx context.Context) ([]model.TgGroup, error) {
 
 		encodedData, err := json.Marshal(fullGroupInfo)
 		if err != nil {
-			g.log.Warn().Err(err)
+			logger.Warn().Err(err).Msg("marshal group data")
+
 			continue
 		}
 
 		err = json.Unmarshal(encodedData, &group)
 		if err != nil {
-			g.log.Warn().Err(err)
+			logger.Warn().Err(err).Msg("Unmarshal group data")
+
 			continue
 		}
 
@@ -59,18 +63,22 @@ func (g tgGroup) GetGroups(ctx context.Context) ([]model.TgGroup, error) {
 }
 
 func (g tgGroup) GetMessagesFromGroupHistory(ctx context.Context, groupPeer *tg.InputPeerChannel) (tg.MessagesMessagesClass, error) {
+	logger := g.log
+
 	groupHistory, err := g.api.MessagesGetHistory(ctx, &tg.MessagesGetHistoryRequest{
 		Peer: groupPeer,
 	})
 	if err != nil {
-		g.log.Error().Err(err)
-		return nil, &errors.GetError{Name: "messages from history", ErrorValue: err}
+		logger.Error().Err(err).Msg("get messages from group history")
+		return nil, fmt.Errorf("get messages from group history error: %w", err)
 	}
 
 	return groupHistory, nil
 }
 
 func (g tgGroup) GetGroupPhoto(ctx context.Context, group *model.TgGroup) (tg.UploadFileClass, error) {
+	logger := g.log
+
 	data, err := g.api.UploadGetFile(ctx, &tg.UploadGetFileRequest{
 		Location: &tg.InputPeerPhotoFileLocation{
 			Peer: &tg.InputPeerChannel{
@@ -82,8 +90,8 @@ func (g tgGroup) GetGroupPhoto(ctx context.Context, group *model.TgGroup) (tg.Up
 		Limit: photo.Size,
 	})
 	if err != nil {
-		g.log.Error().Err(err)
-		return nil, &errors.GetError{Name: "group photo", ErrorValue: err}
+		logger.Error().Err(err).Msg("get group photo")
+		return nil, fmt.Errorf("get group photo error: %w", err)
 	}
 
 	return data, nil
