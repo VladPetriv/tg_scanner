c3c8880e19b979427b7d88802d54b13465b15e9c
Vlad Petriv <vladpetriv05@gmail.com>
2022-12-10 12:18:00 +0200

    chore: remove telegram related logic from app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,20 +5,17 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/gotd/td/telegram"
-
 	"github.com/VladPetriv/tg_scanner/internal/client"
 	"github.com/VladPetriv/tg_scanner/internal/client/auth"
 	"github.com/VladPetriv/tg_scanner/internal/controller"
 	"github.com/VladPetriv/tg_scanner/internal/store"
 	"github.com/VladPetriv/tg_scanner/pkg/config"
 	"github.com/VladPetriv/tg_scanner/pkg/logger"
+	"github.com/gotd/td/telegram"
 )
 
 func Run(store *store.Store, queue controller.Controller, cfg *config.Config, log *logger.Logger) {
-	waitGroup := sync.WaitGroup{}
-
-	tgClient, err := telegram.ClientFromEnvironment(telegram.Options{})
+	tgClient, err := telegram.ClientFromEnvironment(telegram.Options{}) //nolint
 	if err != nil {
 		log.Error().Err(err).Msg("create telegram client")
 	}
@@ -28,10 +25,8 @@ func Run(store *store.Store, queue controller.Controller, cfg *config.Config, lo
 
 	appClient := client.New(ctx, store, queue, api, log, cfg)
 
-	log.Info().Msg("start the application")
-
 	if err := tgClient.Run(ctx, func(ctx context.Context) error {
-		log.Info().Msg("authenticate user")
+		var waitGroup sync.WaitGroup
 
 		user, err := auth.Login(ctx, tgClient, cfg)
 		if err != nil {
@@ -40,71 +35,21 @@ func Run(store *store.Store, queue controller.Controller, cfg *config.Config, lo
 
 		userData, _ := user.GetUser().AsNotEmpty()
 
-		log.Info().Msg("get user groups")
-
-		groups, err := appClient.Groups.GetGroups(ctx)
+		groups, err := appClient.ValidateAndPushGroupsToQueue(ctx)
 		if err != nil {
-			log.Error().Err(err).Msg("get user groups")
+			return fmt.Errorf("failed to validation and push groups to queue: %w", err)
 		}
 
-		log.Info().Msg("create base files")
-		err = appClient.Groups.CreateFilesForGroups(groups)
-		if err != nil {
-			log.Error().Err(err).Msg("create base files for group messages")
-		}
+		waitGroup.Add(3) //nolint
 
-		log.Info().Msg("check if groups are in cache")
-		for _, groupData := range groups {
-			if groupData.ID == 0 {
-				continue
-			}
-
-			groupValue, err := store.Cache.Get(ctx, store.Cache.GenerateKey(groupData))
-			if err != nil {
-				log.Error().Err(err).Msg("get value from cache")
-			}
-
-			if groupValue == "" {
-				err = store.Cache.Set(ctx, store.Cache.GenerateKey(groupData), true)
-				if err != nil {
-					log.Error().Err(err).Msg("set value into cache")
-				}
-			} else {
-				continue
-			}
-
-			groupPhotoData, err := appClient.Groups.GetGroupPhoto(ctx, &groupData)
-			if err != nil {
-				log.Error().Err(err).Msgf("get [%s] photo data", groupData.Username)
-
-				continue
-			}
-
-			groupImageUrl, err := appClient.Photos.ProcessPhoto(ctx, groupPhotoData, groupData.Username)
-			if err != nil {
-				log.Error().Err(err).Msgf("process [%s] photo data", groupData.Username)
-			}
-
-			groupData.ImageURL = groupImageUrl
-
-			err = queue.PushDataToQueue("groups", groupData)
-			if err != nil {
-				log.Error().Err(err).Msgf("push [%s] into queue", groupData.Username)
-			}
-		}
-
-		log.Info().Msg("successfully pushed groups into queue")
-
-		waitGroup.Add(3)
-
-		go appClient.PushToQueue()
+		go appClient.PushMessagesToQueue()
 		go appClient.GetHistoryMessages(groups[5:])
-		go appClient.GetIncomingMessages(userData, groups[5:])
+		go appClient.GetIncomingMessages(*userData, groups[5:])
 
 		waitGroup.Wait()
 
 		return nil
 	}); err != nil {
-		log.Error().Err(err)
+		log.Fatal().Err(err).Msg("start application")
 	}
 }
