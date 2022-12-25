76bcab26de446c6cca11abb3e3fa168480e26650
Vlad Petriv <vladpetriv05@gmail.com>
2022-12-25 12:20:47 +0200

    refactor: storage module by moving and renaming interfaces with files

---
+++ internal/store/cache/cache.go
@@ -0,0 +1,9 @@
+package cache
+
+import "context"
+
+type Store interface {
+	Get(ctx context.Context, key string) (string, error)
+	Set(ctx context.Context, key string, value bool) error
+	GenerateKey(value interface{}) string
+}
