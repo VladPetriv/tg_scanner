14976ba2f2697b57456191585d5f7a932f23015d
Vlad Petriv <vladpetriv05@gmail.com>
2023-03-04 22:36:38 +0100

    refactor: decompose sending image to firebase, change error messagesa

--- internal/store/image/firebase.go
+++ internal/store/image/firebase.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 
+	"cloud.google.com/go/storage"
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
 
@@ -17,60 +18,74 @@ type firebaseStore struct {
 }
 
 func NewFirebase(cfg *config.Config) Store {
-	return &firebaseStore{cfg: cfg}
+	return &firebaseStore{
+		cfg: cfg,
+	}
 }
 
-func (f firebaseStore) Send(ctx context.Context, path string, objectName string) (string, error) {
+func (f firebaseStore) Send(ctx context.Context, pathToImage string, objectName string) (string, error) {
 	defaultURL := fmt.Sprintf(
 		"https://firebasestorage.googleapis.com/v0/b/%s/o/default.jpg?alt=media",
 		f.cfg.StorageBucket,
 	)
-	if objectName == "" || path == "" {
+	if objectName == "" || pathToImage == "" {
 		return defaultURL, nil
 	}
 
-	config := &firebase.Config{
+	bucket, err := f.createFirebaseBucket(ctx)
+	if err != nil {
+		return "", fmt.Errorf("create firebase storage bucket: %w", err)
+	}
+
+	storageWriter := bucket.Object(objectName).NewWriter(ctx)
+
+	image, err := os.Open(pathToImage)
+	if err != nil {
+		return "", fmt.Errorf("open image: %w", err)
+	}
+
+	if _, err = io.Copy(storageWriter, image); err != nil {
+		return "", fmt.Errorf("copy local image to storage writer: %w", err)
+	}
+
+	if err = storageWriter.Close(); err != nil {
+		return "", fmt.Errorf("close storage writer: %w", err)
+	}
+
+	err = os.Remove(pathToImage)
+	if err != nil {
+		return "", fmt.Errorf("delete image: %w", err)
+	}
+
+	return fmt.Sprintf(
+		"https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media",
+		f.cfg.StorageBucket,
+		objectName,
+	), nil
+}
+
+func (f firebaseStore) createFirebaseBucket(ctx context.Context) (*storage.BucketHandle, error) {
+	appConfig := &firebase.Config{
 		ProjectID:     f.cfg.ProjectID,
 		StorageBucket: f.cfg.StorageBucket,
 	}
 
 	opt := option.WithCredentialsFile(f.cfg.SecretPath)
-	app, err := firebase.NewApp(ctx, config, opt)
+
+	app, err := firebase.NewApp(ctx, appConfig, opt)
 	if err != nil {
-		return "", fmt.Errorf("create firebase app error: %w", err)
+		return nil, fmt.Errorf("create firebase app: %w", err)
 	}
 
-	client, err := app.Storage(ctx)
+	appStorage, err := app.Storage(ctx)
 	if err != nil {
-		return "", fmt.Errorf("get access to storage error: %w", err)
+		return nil, fmt.Errorf("create new instance of storage: %w", err)
 	}
 
-	bucket, err := client.DefaultBucket()
+	bucket, err := appStorage.DefaultBucket()
 	if err != nil {
-		return "", fmt.Errorf("get default bucket error: %w", err)
+		return nil, fmt.Errorf("get storage bucket: %w", err)
 	}
 
-	storageWriter := bucket.Object(objectName).NewWriter(ctx)
-
-	image, err := os.Open(path)
-	if err != nil {
-		return "", fmt.Errorf("opening image error: %w", err)
-	}
-
-	if _, err = io.Copy(storageWriter, image); err != nil {
-		return "", fmt.Errorf("copying image to firebase storage error: %w", err)
-	}
-
-	if err = storageWriter.Close(); err != nil {
-		return "", fmt.Errorf("closing firebase storage writer error: %w", err)
-	}
-
-	url := fmt.Sprintf("https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media", f.cfg.StorageBucket, objectName)
-
-	err = os.Remove(path)
-	if err != nil {
-		return "", fmt.Errorf("delete image error: %w", err)
-	}
-
-	return url, nil
+	return bucket, nil
 }
