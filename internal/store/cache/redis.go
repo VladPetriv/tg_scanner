19e64b4bf85136b6aed62ce1d9393df33a5c67cb
Vlad Petriv <vladpetriv05@gmail.com>
2023-03-04 20:11:42 +0100

    refactor: add check for implementation of interface, update error messages

--- internal/store/cache/redis.go
+++ internal/store/cache/redis.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
 
-	"github.com/VladPetriv/tg_scanner/internal/model"
-	"github.com/VladPetriv/tg_scanner/pkg/config"
+	"github.com/VladPetriv/tg_scanner/config"
 	"github.com/go-redis/redis/v9"
 )
 
@@ -16,7 +14,9 @@ type redisStore struct {
 	client *redis.Client
 }
 
-func NewRedis(cfg *config.Config) Store {
+var _ Store = (*redisStore)(nil)
+
+func NewRedis(cfg *config.Config) *redisStore {
 	client := redis.NewClient(&redis.Options{
 		Addr:     cfg.RedisAddr,
 		Password: cfg.RedisPassword,
@@ -26,39 +26,24 @@ func NewRedis(cfg *config.Config) Store {
 	return &redisStore{cfg: cfg, client: client}
 }
 
-func (r redisStore) Get(ctx context.Context, data interface{}) (string, error) {
-	value, err := r.client.Get(ctx, generateKey(data)).Result()
-	if err != nil && !errors.Is(err, redis.Nil) {
-		return "", fmt.Errorf("get data from redis error: %w", err)
+func (r redisStore) Get(ctx context.Context, key string) (string, error) {
+	value, err := r.client.Get(ctx, key).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return "", nil
+		}
+
+		return "", fmt.Errorf("get data from redis: %w", err)
 	}
 
 	return value, nil
 }
 
-func (r redisStore) Set(ctx context.Context, data interface{}, value bool) error {
-	err := r.client.Set(ctx, generateKey(data), value, 0)
+func (r redisStore) Set(ctx context.Context, key string, value bool) error {
+	err := r.client.Set(ctx, key, value, 0)
 	if err.Err() != nil {
-		return fmt.Errorf("set data to redis error: %w", err.Err())
+		return fmt.Errorf("set data to redis: %w", err.Err())
 	}
 
 	return nil
-}
-
-func generateKey(value interface{}) string {
-	var key string
-
-	switch data := value.(type) {
-	case model.TgMessage:
-		key = fmt.Sprintf(
-			"[%d%d%d-%s]",
-			data.ID,
-			data.FromID.ID,
-			data.PeerID.ID,
-			strings.ReplaceAll(data.Message, " ", ""),
-		)
-	case model.TgGroup:
-		key = fmt.Sprintf("[%d%s]", data.ID, data.Username)
-	}
-
-	return key
 }
--- internal/store/image/firebase.go
+++ internal/store/image/firebase.go
@@ -9,23 +9,25 @@ import (
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
 
-	"github.com/VladPetriv/tg_scanner/pkg/config"
+	"github.com/VladPetriv/tg_scanner/config"
 )
 
 type firebaseStore struct {
 	cfg *config.Config
 }
 
-func NewFirebase(cfg *config.Config) Store {
+var _ Store = (*firebaseStore)(nil)
+
+func NewFirebase(cfg *config.Config) *firebaseStore {
 	return &firebaseStore{cfg: cfg}
 }
 
-func (f firebaseStore) Send(ctx context.Context, path string, objectName string) (string, error) {
+func (f firebaseStore) Send(ctx context.Context, pathToImage string, objectName string) (string, error) {
 	defaultURL := fmt.Sprintf(
 		"https://firebasestorage.googleapis.com/v0/b/%s/o/default.jpg?alt=media",
 		f.cfg.StorageBucket,
 	)
-	if objectName == "" || path == "" {
+	if objectName == "" || pathToImage == "" {
 		return defaultURL, nil
 	}
 
@@ -37,39 +39,43 @@ func (f firebaseStore) Send(ctx context.Context, path string, objectName string)
 	opt := option.WithCredentialsFile(f.cfg.SecretPath)
 	app, err := firebase.NewApp(ctx, config, opt)
 	if err != nil {
-		return "", fmt.Errorf("create firebase app error: %w", err)
+		return "", fmt.Errorf("create firebase app: %w", err)
 	}
 
 	client, err := app.Storage(ctx)
 	if err != nil {
-		return "", fmt.Errorf("get access to storage error: %w", err)
+		return "", fmt.Errorf("get access to app storage: %w", err)
 	}
 
 	bucket, err := client.DefaultBucket()
 	if err != nil {
-		return "", fmt.Errorf("get default bucket error: %w", err)
+		return "", fmt.Errorf("get default bucket: %w", err)
 	}
 
 	storageWriter := bucket.Object(objectName).NewWriter(ctx)
 
-	image, err := os.Open(path)
+	image, err := os.Open(pathToImage)
 	if err != nil {
-		return "", fmt.Errorf("opening image error: %w", err)
+		return "", fmt.Errorf("open image: %w", err)
 	}
 
 	if _, err = io.Copy(storageWriter, image); err != nil {
-		return "", fmt.Errorf("copying image to firebase storage error: %w", err)
+		return "", fmt.Errorf("copy image to firebase storage writer: %w", err)
 	}
 
 	if err = storageWriter.Close(); err != nil {
-		return "", fmt.Errorf("closing firebase storage writer error: %w", err)
+		return "", fmt.Errorf("close firebaser storage writer: %w", err)
 	}
 
-	url := fmt.Sprintf("https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media", f.cfg.StorageBucket, objectName)
+	url := fmt.Sprintf(
+		"https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media",
+		f.cfg.StorageBucket,
+		objectName,
+	)
 
-	err = os.Remove(path)
+	err = os.Remove(pathToImage)
 	if err != nil {
-		return "", fmt.Errorf("delete image error: %w", err)
+		return "", fmt.Errorf("delete image: %w", err)
 	}
 
 	return url, nil
